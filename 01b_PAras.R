33a09f6a32c918990fa53b0324c897f7d7ae6bce
clittlef <clittlef@uw.edu>
2019-12-23 13:39:24 -0800

    aggregation to keep only pixels with 75% PA

--- 01b_PAras.R
+++ 01b_PAras.R
@@ -2,12 +2,6 @@
 #### RASTERIZING ####
 #####################
 
-## Load raster template
-# ecoregion template
-ecoreg.r <- raster(paste0(data.dir,"ecoregions/ecoregion raster.tif"))
-plot(ecoreg.r)
-
-
 
 #### OVERLAPS? ######################################################
 ## Before fasterizing, how big of an issue will overlaps be?
@@ -24,9 +18,9 @@ redwood <- PA.IVInoass.1km %>%
 bbox <- st_bbox(redwood)
 # -124.20333   37.00931 -122.03447   41.84049
 # Make smaller bbox
-df <- data.frame(lon=c(-124, -124, -123, -123),
+df <- data.frame(lon=c(-124, -124, -122.5, -122.5),
                  # lat=c(39, 41, 41, 39))
-                 lat=c(40, 40.5, 40.5, 40))
+                 lat=c(37, 41.5, 37, 41.5))
 # Need to close it: 1st & last pts must be identical
 df <- rbind(df, df[1,])
 bbox <- st_bbox(st_sf(st_sfc(st_polygon(list(as.matrix(df)))), crs = st_crs(redwood)))
@@ -36,18 +30,19 @@ plot(st_geometry(redwood),
      col = sf.colors(nrow(redwood)))
 
 
+
 ## Try fasterizing, retaining ID, default fun (last); see how many dropped
 # redwoods.r <- fasterize(redwood, ecoreg.r, field = "ID")
 # length(unique(getValues(redwoods.r))) # 8  unique IDs
 # # NA 57281 52178 55146 37805 50794 46733 42538
 # # ^ 8 IDs relative to 19 observations means some got dropped (overlap)
 
-redwoods.r <- fasterize(redwood, ecoreg.r, field = "ID", fun = "max")
-length(unique(getValues(redwoods.r))) # 8  unique IDs
-# NA 57281 52178 55146 37805 50794 46733 42538
-# ^ Those unique IDs match the ones with last; i.e., order in which polygons drawn?
-# Prairie Creek Redwoods SP (49094) is contained by NP (57281).
-# So former gets dropped. Prove with summation approach below.
+# redwoods.r <- fasterize(redwood, ecoreg.r, field = "ID", fun = "max")
+# length(unique(getValues(redwoods.r))) # 8  unique IDs
+# # NA 57281 52178 55146 37805 50794 46733 42538
+# # ^ Those unique IDs match the ones with last; i.e., order in which polygons drawn?
+# # Prairie Creek Redwoods SP (49094) is contained by NP (57281).
+# # So former gets dropped. Prove with summation approach below.
 
 # redwoods.r <- fasterize(redwood, ecoreg.r, field = "ID", fun = "sum")
 # length(unique(getValues(redwoods.r))) # 11  unique IDs
@@ -58,9 +53,15 @@ length(unique(getValues(redwoods.r))) # 8  unique IDs
 
 
 
+
 #### TEMPLATE ############################################################
 
 ## Create higher res raster for PA conversion, based on ecoreg
+# Load ecoregion raster template
+ecoreg.r <- raster(paste0(data.dir,"ecoregions/ecoregion raster.tif"))
+plot(ecoreg.r)
+
+
 # Will want to retain  pixels with at least 75% PA
 extent(ecoreg.r)
 crs(ecoreg.r)
@@ -72,6 +73,9 @@ ref.ras <- raster(extent(ecoreg.r),
                   vals =1)
 
 crs <- paste0(crs(ref.ras))
+rem(ecoreg.r)
+
+
 
 
 
@@ -87,58 +91,79 @@ PA.IVInoass.1km <- st_transform(PA.IVInoass.1km, crs); st_crs(PA.IVInoass.1km)
 
 
 
+
+
 #### RASTERIZE ############################################################
 
 ## Rasterize with fasterize. Specify ID as field to retain. 
 # Background default is NA.
-PA.IVI.r <- fasterize(PA.IVI.1km, raster = ref.ras, field = "ID", fun = "max")
-PA.IVInoass.r <- fasterize(PA.IVInoass.1km, raster = ref.ras, field = "ID", fun = "max")
+PA.IVI.r <- fasterize(PA.IVI.1km,
+                      raster = ref.ras, field = "ID", fun = "max")
+PA.IVInoass.r <- fasterize(PA.IVInoass.1km,
+                           raster = ref.ras, field = "ID", fun = "max")
 
-#### START HERE!!!!
 
+plot(PA.IVI.r, xlim=c(bbox[1], bbox[3]), ylim=c(bbox[2], bbox[4]))
+plot(PA.IVInoass.r, xlim=c(bbox[1], bbox[3]), ylim=c(bbox[2], bbox[4]))
 
-par(mfrow=c(1,2)) # Look around Yellowstone-Grand Teton
-plot(PA.IVI.r, xlim=c(-9220000,-9040000), ylim=c(5350000,5590000))
-plot(PA.IVInoass.r, xlim=c(-9220000,-9040000), ylim=c(5350000,5590000))
-plot(PA.IVI.r, xlim=c(-9220000,-9040000), ylim=c(5350000,5590000))
-par(mfrow=c(1,1))
 
 # res should be the only parameter different: template coarser.
-extent(PA.IIV.r) ; extent(PA.IV.r) ; extent(PA.IVI.r) ; extent(template.r)
-crs(PA.IIV.r) ; crs(PA.IV.r); crs(PA.IVI.r) ; crs(template.r)
-res(PA.IIV.r) ; res(PA.IV.r); res(PA.IVI.r); res(template.r)
+extent(PA.IVI.r) ; extent(PA.IVInoass.r) ; extent(ref.ras) #; extent(ecoreg.r)
+crs(PA.IVI.r) ; crs(PA.IVInoass.r); crs(ref.ras) #; crs(ecoreg.r)
+res(PA.IVI.r) ; res(PA.IVInoass.r); res(ref.ras) #; res(ecoreg.r)
 
 
-#####################################################
-## Aggregate & align with climate raster. Keep only 75% PA pixels
-# Make sure all PAs are only 1
-PA.IIV.r[! is.na(PA.IIV.r)] <- 1
-PA.IV.r[! is.na(PA.IV.r)] <- 1
-PA.IVI.r[! is.na(PA.IVI.r)] <- 1
 
-## Aggregate by factor of 2 (2000x2000 --> 4000x4000) and take sum.
-# Sum is proxy for how much of clim pixel has PA. Want at least 75%.
-# All PA areas have val of 1, so sum for keeping should be 3 or 4.
+
+#### AGGREGATE ##########################################
+## Aggregate & align with template raster. Keep only 75% PA pixels.
+# Want to maintain IDs of PAs, so create second PA template w/ any PA = 1
+# Then will aggregate by mode with IDs and clip output based on that PA template.
+
+## Create PA=1 templates
+temp.PA.IVI.r <- PA.IVI.r
+temp.PA.IVInoass.r <- PA.IVInoass.r
+temp.PA.IVI.r[! is.na(temp.PA.IVI.r)] <- 1
+temp.PA.IVInoass.r[! is.na(temp.PA.IVInoass.r)] <- 1
+
+## Aggregate these by factor of 2 and sum. Keep pixels with 3 or 4 (GTE 75% PA)
 start <- Sys.time()
-PA.IIV.r.agg <- aggregate(PA.IIV.r, fact = 2, fun = "sum")
-PA.IV.r.agg <- aggregate(PA.IV.r, fact = 2, fun = "sum")
-PA.IVI.r.agg <- aggregate(PA.IVI.r, fact = 2, fun = "sum")
+temp.PA.IVI.r.agg <- aggregate(temp.PA.IVI.r, fact = 2, fun = "sum")
+temp.PA.IVInoass.r.agg <- aggregate(temp.PA.IVInoass.r, fact = 2, fun = "sum")
+print(paste0("Done aggregating"))
+print(Sys.time() - start)
+
+## Retain only pixels (0.04166667x0.04166667) that have at least 75% area in PA.
+# Set all values < 3 to NA; retain all GTE 3 as 1
+temp.PA.IVI.r.agg[temp.PA.IVI.r.agg < 3] <- NA
+temp.PA.IVI.r.agg[temp.PA.IVI.r.agg >= 3] <- 1
+temp.PA.IVInoass.r.agg[temp.PA.IVInoass.r.agg < 3] <- NA
+temp.PA.IVInoass.r.agg[temp.PA.IVInoass.r.agg >= 3] <- 1
+
+
+
+## Aggregate ID-ed rasters by factor of 2 and keep majority PA.
+# (0.02083333x0.02083333 --> 0.04166667x0.04166667).
+# N.b., don't set na.rm=FALSE b/c ANY NA returns NA, even if not mode.
+start <- Sys.time()
+PA.IVI.r.agg <- aggregate(PA.IVI.r, fact = 2, fun = modal)
+PA.IVInoass.r.agg <- aggregate(PA.IVInoass.r, fact = 2, fun = modal)
 print(paste0("Done aggregating"))
 print(Sys.time() - start) 
 
+plot(PA.IVI.r.agg, xlim=c(bbox[1], bbox[3]), ylim=c(bbox[2], bbox[4]))
+zoom(PA.IVI.r.agg, xlim=c(bbox[1], bbox[3]), ylim=c(bbox[2], bbox[4]))
+plot(PA.IVInoass.r.agg, xlim=c(bbox[1], bbox[3]), ylim=c(bbox[2], bbox[4]))
+
+
+
+## Raster math and only retain the ID's PAs that coincide w/ an "in" pixel.
 # Save for comparisons
 PA.IIV.r.agg.orig <- PA.IIV.r.agg
 PA.IV.r.agg.orig <- PA.IV.r.agg
 PA.IVI.r.agg.orig <- PA.IVI.r.agg
 
-## Retain only pixels (4000x4000) that have at least 75% area in PA.
-# Set all values < 3 to NA; retain all GTE 3 as 1
-PA.IIV.r.agg[PA.IIV.r.agg < 3] <- NA
-PA.IIV.r.agg[PA.IIV.r.agg >= 3] <- 1
-PA.IV.r.agg[PA.IV.r.agg < 3] <- NA
-PA.IV.r.agg[PA.IV.r.agg >= 3] <- 1
-PA.IVI.r.agg[PA.IVI.r.agg < 3] <- NA
-PA.IVI.r.agg[PA.IVI.r.agg >= 3] <- 1
+
 
 ## DO NOT RESAMPLE TO TEMPLATE.R ELSE COARSEN ##
 
